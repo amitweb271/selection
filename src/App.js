07f84c6829e3afc8e4951b20679da55c40537100
amitweb271 <63352592+amitweb271@users.noreply.github.com>
2022-07-07 18:54:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Options from './components/Options';
+import Product from './components/Product';
+
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Options />} />
+        <Route path="/task2" element={<Product />} />
       </Routes>
     </BrowserRouter >
   );
